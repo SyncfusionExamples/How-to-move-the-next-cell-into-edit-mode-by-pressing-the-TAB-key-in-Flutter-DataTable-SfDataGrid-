4f5c962c8d49e6f7513ee409866b5bbb2ee08093
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2023-01-12 18:15:09 +0530

    The Sample has been added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,78 @@
-# How-to-move-the-next-cell-into-edit-mode-by-pressing-the-tab-key-in-Flutter-DataTable-SfDataGrid-
+// ignore_for_file: depend_on_referenced_packages
 
-The [Flutter DataGrid](https://www.syncfusion.com/flutter-widgets/flutter-datagrid) provides support for moving a cell into edit mode programmatically by calling the [DataGridController.beginEdit](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/DataGridController/beginEdit.html) method. In this article, you can learn about how to move the next cell into edit mode by pressing the tab key on the keyboard.
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:collection/collection.dart';
 
-## STEP 1: 
-Create a custom selection manager class by extending the [RowSelectionManager](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/RowSelectionManager-class.html). In the [handleKeyEvent](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/RowSelectionManager/handleKeyEvent.html) method, call the `DataGridController.beginEdit` inside the WidgetsBinding.instance.addPostFrameCallback using the [DataGridController.currentCell](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/DataGridController/currentCell.html).
+void main() {
+  runApp(const MaterialApp(home: SfDataGridDemo()));
+}
 
-```dart
+class SfDataGridDemo extends StatefulWidget {
+  const SfDataGridDemo({Key? key}) : super(key: key);
+
+  @override
+  SfDataGridDemoState createState() => SfDataGridDemoState();
+}
+
+class SfDataGridDemoState extends State<SfDataGridDemo> {
+  List<Employee> _employees = <Employee>[];
+  late EmployeeDataSource _employeeDataSource;
+  DataGridController dataGridController = DataGridController();
+  late CustomSelectionManager customSelectionManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _employees = getEmployeeData();
+    _employeeDataSource = EmployeeDataSource(_employees, context);
+    customSelectionManager = CustomSelectionManager(dataGridController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Syncfusion Flutter DataGrid')),
+      body: SfDataGrid(
+          source: _employeeDataSource,
+          controller: dataGridController,
+          allowEditing: true,
+          selectionManager: customSelectionManager,
+          navigationMode: GridNavigationMode.cell,
+          selectionMode: SelectionMode.single,
+          columns: <GridColumn>[
+            GridColumn(
+                columnName: 'id',
+                label: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.centerRight,
+                    child: const Text('ID'))),
+            GridColumn(
+                columnName: 'name',
+                label: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.centerLeft,
+                    child: const Text('Name'))),
+            GridColumn(
+                columnName: 'designation',
+                label: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.centerLeft,
+                    child: const Text('Designation',
+                        overflow: TextOverflow.ellipsis))),
+            GridColumn(
+                columnName: 'salary',
+                label: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.centerRight,
+                    child: const Text('Salary'))),
+          ],
+          columnWidthMode: ColumnWidthMode.fill),
+    );
+  }
+}
+
 class CustomSelectionManager extends RowSelectionManager {
   CustomSelectionManager(this.dataGridController);
   DataGridController dataGridController;
@@ -20,11 +87,6 @@ class CustomSelectionManager extends RowSelectionManager {
   }
 }
 
-```
-## STEP 2: 
-Create a data source class by extending the [DataGridSource](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/DataGridSource-class.html) for mapping data to the SfDataGrid and add the required edit widget through the [DataGridSource.buildEditWidget](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/DataGridSource/buildEditWidget.html) method in the DataGridSource class.
-
-```dart
 class EmployeeDataSource extends DataGridSource {
   EmployeeDataSource(this.employees, this.buildContext) {
     dataGridRows = employees
@@ -163,64 +225,35 @@ class EmployeeDataSource extends DataGridSource {
   }
 }
 
-```
-## STEP 3: 
-Initialize the [SfDataGrid](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/SfDataGrid-class.html) widget with all the required properties. Create an instance of the CustomSelectionManager class and assign it to the [SfDataGrid.selectionManager](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/SfDataGrid/selectionManager.html) property. Create an instance of the DataGridController and assign it to the [SfDataGrid.controller](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/SfDataGrid/controller.html) property.
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
 
-```dart
-List<Employee> _employees = <Employee>[];
-  late EmployeeDataSource _employeeDataSource;
-  DataGridController dataGridController = DataGridController();
-  late CustomSelectionManager customSelectionManager;
+  int? id;
+  String? name;
+  String? designation;
+  int? salary;
 
-  @override
-  void initState() {
-    super.initState();
-    _employees = getEmployeeData();
-    _employeeDataSource = EmployeeDataSource(_employees, context);
-    customSelectionManager = CustomSelectionManager(dataGridController);
+  DataGridRow getDataGridRow() {
+    return DataGridRow(cells: <DataGridCell>[
+      DataGridCell<int>(columnName: 'id', value: id),
+      DataGridCell<String>(columnName: 'name', value: name),
+      DataGridCell<String>(columnName: 'designation', value: designation),
+      DataGridCell<int>(columnName: 'salary', value: salary),
+    ]);
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Syncfusion Flutter DataGrid')),
-      body: SfDataGrid(
-          source: _employeeDataSource,
-          controller: dataGridController,
-          allowEditing: true,
-          selectionManager: customSelectionManager,
-          navigationMode: GridNavigationMode.cell,
-          selectionMode: SelectionMode.single,
-          columns: <GridColumn>[
-            GridColumn(
-                columnName: 'id',
-                label: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    alignment: Alignment.centerRight,
-                    child: const Text('ID'))),
-            GridColumn(
-                columnName: 'name',
-                label: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    alignment: Alignment.centerLeft,
-                    child: const Text('Name'))),
-            GridColumn(
-                columnName: 'designation',
-                label: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    alignment: Alignment.centerLeft,
-                    child: const Text('Designation',
-                        overflow: TextOverflow.ellipsis))),
-            GridColumn(
-                columnName: 'salary',
-                label: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    alignment: Alignment.centerRight,
-                    child: const Text('Salary'))),
-          ],
-          columnWidthMode: ColumnWidthMode.fill),
-    );
-  }
-
-```
+List<Employee> getEmployeeData() {
+  return [
+    Employee(10001, 'Lara', 'Project Lead', 45000),
+    Employee(10002, 'James', 'Developer', 23000),
+    Employee(10003, 'Kathryn', 'Project Lead', 43500),
+    Employee(10004, 'Michael', 'Manager', 50000),
+    Employee(10005, 'Martin', 'Developer', 25000),
+    Employee(10006, 'Jack', 'Project Lead', 54000),
+    Employee(10007, 'Balnc', 'UI Designer', 28500),
+    Employee(10008, 'Lara', 'Project Lead', 40000),
+    Employee(10009, 'James', 'Developer', 23000),
+    Employee(10010, 'Kathryn', 'Project Lead', 43500)
+  ];
+}
